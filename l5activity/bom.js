774eaa0ccea7dce0faf2efd472004f2079a39a1e
makenaduncan <dun19011@byui.edu>
2021-10-14 14:50:18 -0600

    added footer

--- l5activity/bom.js
+++ l5activity/bom.js
@@ -29,3 +29,28 @@ function pressButton() {
 
     input.focus();
 }
+
+// Current year for copyright in Footer
+const yearspan = document.getElementById("year")
+
+let currentDate = new Date();
+
+yearspan.innerHTML = currentDate.getFullYear(); 
+
+
+const lastModified = new Date(document.lastModified);
+
+// Last Updated Code for Footer
+var weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday",
+"Thursday", "Friday", "Saturday");
+var months = new Array("January", "February", "March",
+"April", "May", "June", "July", "August", "September",
+ "October", "November", "December");
+var day = lastModified.getDay();
+var day = weekday[day];
+var month = lastModified.getMonth();
+var month = months[month];
+
+var lastUpdated = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
+
+document.getElementById("lastModified").textContent = lastUpdated;
