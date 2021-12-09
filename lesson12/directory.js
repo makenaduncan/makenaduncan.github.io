703f9a0f9ee39c604076248ff912f6d40cfaa41d
makena <mduncan18@byui.edu>
2021-12-08 22:17:09 -0700

    second commit

--- lesson12/directory.js
+++ lesson12/directory.js
@@ -1,65 +1,4 @@
-    //request json for business info
-    // const requestURL = 'directory.json';
-
-    // fetch(requestURL)
-    //     .then(function(response) {
-    //         if (response.ok) {
-    //             return response.json();
-    //         }
-    //         throw new ERROR('Network response was not ok');
-    //     })
-    //     .then(function(jsonObject) {
-    //         console.table(jsonObject); // temporary checking for valid response and data parsing
-    //         const businesses = jsonObject['businesses'];
-    
-    
-    //         for (let i = 0; i < businesses.length; i++) {
-    //             let name = businesses[i].name;
-    //             name = String(name);
-    //               //create section element with class of 'card'
-    //               let card = document.createElement('section');
-    //               card.classList.add('card');
-    //               //create div element with class of 'data'
-    //               let data = document.createElement('div');
-    //               data.classList.add('data');
-    //               //create different elements
-    //               let h2 = document.createElement('h2');
-    //               let cp = document.createElement('p');
-    //               let ar = document.createElement('p');
-    //               let index = document.createElement('p');
-    //               let image = document.createElement('img');
-    
-    //               // add appropriate content to elements
-    //               h2.textContent = businesses[i].name;
-    //               index.textContent = businesses[i].motto;
-    //               cp.textContent = 'Contact business here: ' + businesses[i].contact;
-    //               ar.textContent = 'Website: ' + businesses[i].website;
-    //               if (businesses[i].photo != "null") {
-    //                 image.setAttribute('src', businesses[i].photo);
-    //                 image.setAttribute('alt', businesses[i].name + ' logo');
-    //               }
-    
-    //               //add elements to have correct children
-    //               card.appendChild(h2);
-    //               if (businesses[i].photo != "null") {
-    //                 card.appendChild(image);
-    //               }
-    //               card.appendChild(data);
-    //               data.appendChild(index);
-    //               data.appendChild(cp);
-    //               data.appendChild(ar);
-                  
-    //               document.querySelector('.cards').appendChild(card);
-                
-    //         }
-    //     })
-    //     .catch(function(error) {
-    //         console.log('Fetch error: ', error.message);
-    //     })
-
-// ------------------------ DIRECTORY PAGE ---------------------------
-
-//change the view with toggle button
+//list-grid toggle button
 var cards = document.getElementById("cards");
 
 document.addEventListener("click", function (event) {
@@ -78,7 +17,7 @@ document.addEventListener("click", function (event) {
   cards.classList.remove("list");
 });
 
-//request json for business info
+// Pulling info from json file
 const requestURL = 'directory.json';
 
 fetch(requestURL)
@@ -96,20 +35,19 @@ fetch(requestURL)
         for (let i = 0; i < businesses.length; i++) {
             let name = businesses[i].name;
             name = String(name);
-              //create section element with class of 'card'
+              
               let card = document.createElement('section');
               card.classList.add('card');
-              //create div element with class of 'data'
+              
               let data = document.createElement('div');
               data.classList.add('data');
-              //create different elements
+              
               let h2 = document.createElement('h2');
               let cp = document.createElement('p');
               let ar = document.createElement('p');
               let index = document.createElement('p');
               let image = document.createElement('img');
 
-              // add appropriate content to elements
               h2.textContent = businesses[i].name;
               index.textContent = businesses[i].motto;
               cp.textContent = 'Contact business here: ' + businesses[i].contact;
@@ -119,7 +57,6 @@ fetch(requestURL)
                 image.setAttribute('alt', businesses[i].name + ' logo');
               }
 
-              //add elements to have correct children
               card.appendChild(h2);
               if (businesses[i].photo != "null") {
                 card.appendChild(image);
